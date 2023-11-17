e7e5288c56f555817272514200a1b581eeaf7419
Kamil Kuziora <kam.kuziora@gmail.com>
2023-11-17 11:23:18 +0100

    disabled scroll when menu oppened

--- project_1/js/menu.js
+++ project_1/js/menu.js
@@ -3,8 +3,9 @@
 const burgerMenuElement = document.querySelector(".header_nav_burger");
 burgerMenuElement.addEventListener("click", () => {
   const mobileMenuElement = document.querySelector(".header_menu_bg");
-  let navigationElement = document.querySelector('.header_nav');
-  let contactElement = document.querySelector('.header_contact');
+  let navigationElement = document.querySelector(".header_nav");
+  let contactElement = document.querySelector(".header_contact");
+  const bodyElement = document.body;
 
   navigationElement = navigationElement.cloneNode(true);
   contactElement = contactElement.cloneNode(true);
@@ -14,11 +15,12 @@ burgerMenuElement.addEventListener("click", () => {
     mobileMenuElement.dataset.active = true;
     mobileMenuElement.appendChild(navigationElement);
 
-    navigationElement.classList.remove('header_nav');
-    navigationElement.classList.add('header_nav_mobile');
+    navigationElement.classList.remove("header_nav");
+    navigationElement.classList.add("header_nav_mobile");
 
-    contactElement.classList.remove('header_contact');
-    contactElement.classList.add('header_contact_mobile');
+    contactElement.classList.remove("header_contact");
+    contactElement.classList.add("header_contact_mobile");
+    bodyElement.style.overflowY = "hidden";
 
     mobileMenuElement.appendChild(contactElement);
   }
@@ -26,10 +28,12 @@ burgerMenuElement.addEventListener("click", () => {
 
 const closeBurgerMenuBtn = document.querySelector(".header_menu_bg_exit_btn");
 closeBurgerMenuBtn.addEventListener("click", () => {
+  const bodyElement = document.body;
   const mobileMenuElement = document.querySelector(".header_menu_bg");
 
   if (mobileMenuElement.dataset.active) {
     mobileMenuElement.dataset.active = false;
-    mobileMenuElement.querySelector('.header_nav_mobile').remove();
+    mobileMenuElement.querySelector(".header_nav_mobile").remove();
+    bodyElement.style.overflowY = "auto";
   }
 });
