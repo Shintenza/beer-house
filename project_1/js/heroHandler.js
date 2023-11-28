c508c6f411d4ac4f8ae1c7a7ac2ee485994daeb4
Kamil Kuziora <kam.kuziora@gmail.com>
2023-11-28 10:31:27 +0100

    added comments to the HeroHandler class

--- project_1/js/heroHandler.js
+++ project_1/js/heroHandler.js
@@ -1,8 +1,16 @@
+/**
+  * class responsible for handling a hero section on the main page
+  * @class
+  */
 class HeroHandler {
   #avaliableSlides = [];
   #currentSlide = 0;
   #slidesList = null;
 
+  /**
+  * @constructor
+  * @param {string} - hero section class name
+  */
   constructor(heroSectionClass) {
     this.#slidesList = document.querySelector(`.${heroSectionClass}`);
     this.#avaliableSlides = [...this.#slidesList.children];
@@ -11,6 +19,10 @@ class HeroHandler {
     setInterval(this.#handleHeroSlides.bind(this), 7000);
   }
 
+  /**
+   * method responsible for changing images in hero section
+   * @method
+  */
   #handleHeroSlides() {
     const currentActiveElement = document.querySelector(".active_slide");
 
